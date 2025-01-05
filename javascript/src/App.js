93376663ce11296251f1c285bdd06a50cb67defb
Chandra6388 <cppatel6388@gmail.com>
2025-01-05 20:09:05 +0530

    Add initial TypeScript component and setup files for React application

--- javascript/src/App.js
+++ javascript/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import StringMethods from './Component/StringMethods';
 import ArrayMethods from './Component/ArrayMethods';
+import TypeScript from './Component/TypeScript';
 
 function App() {
   return (
     <div className="App">
       {/* <StringMethods /> */}
-      <ArrayMethods />
+      <TypeScript />
+      {/* <ArrayMethods /> */}
     </div>
   );
 }
---
+++ javascript/src/Component/TypeScript.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+const StringMethods = () => {
+    const str = "Hello World!";
+
+
+    return (
+        <div>
+            <h1>String Methods</h1>
+        </div>
+    );
+}
+
+export default StringMethods;
